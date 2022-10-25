14241dd0fad622ea21ae0ea67259434a0729af7b
aljoantony24@gmail.com <aljoantony24@gmail.com>
2022-10-25 16:35:54 +0530

    updated

--- Admin/magazine.php
+++ Admin/magazine.php
@@ -33,7 +33,7 @@
                 $extension = explode('.',$fname);
                 $extension = strtolower(end($extension));  
                 $fnew = uniqid().'.'.$extension;
-                $store = "../Upload/magazine/".basename($fnew);                    
+                $store = "/Upload/magazine/".basename($fnew);                    
         if($extension == 'jpg'||$extension == 'png'||$extension == 'gif' )
          {        
             if($fsize>=100000000)
@@ -327,7 +327,7 @@
                                      echo ' <tr>
                                             <td>'.$rows['mag_id'].'</td>
                                             <td>'.$rows['mag_name'].'</td>
-                                            <td>'.$rows['mag_image'].'</td>
+                                            <td>'.$rows['mag_img'].'</td>
                                             <td>'.$rows['mag_cat_name'].'</td>
                                             <td>'.$rows['mag_file'].'</td>
                                             <td>'.$rows['mag_date'].'</td>
